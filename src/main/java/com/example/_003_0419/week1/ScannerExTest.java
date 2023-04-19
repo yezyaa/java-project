ab04b39cad0eefa378cd8879376a220669776333
yezyaa <iyeahji@gmail.com>
2023-04-19 15:29:03 +0900

    feat: Scanner 사용해서 두 개의 숫자 더하기 - ScannerEx, ScannerExTest 추가

---
+++ src/main/java/com/example/_003_0419/week1/ScannerExTest.java
@@ -0,0 +1,8 @@
+package com.example._003_0419.week1;
+
+public class ScannerExTest {
+    public static void main(String[] args) {
+        ScannerEx se = new ScannerEx();
+        se.readTwoNumbersAndPlus();
+    }
+}
