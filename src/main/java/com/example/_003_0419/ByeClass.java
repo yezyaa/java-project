fd795c4a7bd9e50623f86c16f0e2a9f926e288be
yezyaa <iyeahji@gmail.com>
2023-04-19 11:31:49 +0900

    feat: Bye기능 추가 - ByeClass, ByeClassTest 추가

---
+++ src/main/java/com/example/_003_0419/ByeClass.java
@@ -0,0 +1,7 @@
+package com.example._003_0419;
+
+public class ByeClass {
+    public void bye() {
+        System.out.println("Bye");
+    }
+}
---
+++ src/main/java/com/example/_003_0419/ByeClassTest.java
@@ -0,0 +1,8 @@
+package com.example._003_0419;
+
+public class ByeClassTest {
+    public static void main(String[] args) {
+        ByeClass byeClass = new ByeClass();
+        byeClass.bye();
+    }
+}
