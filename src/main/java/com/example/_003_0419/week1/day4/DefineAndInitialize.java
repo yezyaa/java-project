be5fece4fff0de8d4a51c8df4f7993955ec42de6
yezyaa <iyeahji@gmail.com>
2023-04-20 15:28:05 +0900

    feat: 변수 선언과 초기화를 한번에 - DefineAndInitialize 추가

---
+++ src/main/java/com/example/_003_0419/week1/day4/DefineAndInitialize.java
@@ -0,0 +1,13 @@
+package com.example._003_0419.week1.day4;
+
+import com.example._003_0419.PrintHello;
+
+import java.util.Scanner;
+
+public class DefineAndInitialize {
+    public static void main(String[] args) {
+        int iVal = 1;
+        PrintHello printHello = new PrintHello();
+        Scanner sc = new Scanner(System.in);
+    }
+}
