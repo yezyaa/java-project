cd974a0205f06f56a833ae1f2e19c5a998f7bdd7
yezyaa <iyeahji@gmail.com>
2023-04-20 13:12:48 +0900

    feat: printf 사용해서 2단 출력 - MultiplicationTable 수정

--- src/main/java/com/example/_003_0419/week1/day4/MultiplicationTable.java
+++ src/main/java/com/example/_003_0419/week1/day4/MultiplicationTable.java
@@ -4,7 +4,14 @@ public class MultiplicationTable {
     public static void main(String[] args) {
         // 2 * 1 = 2
         // 2 * 2 = 4
-        System.out.println("2" + " * " + "1" + " = " + "2");
-        System.out.printf("%d * %d = %d", 2, 2, 4);
+        System.out.printf("%d * %d = %d\n", 2, 1, 2);
+        System.out.printf("%d * %d = %d\n", 2, 2, 4);
+        System.out.printf("%d * %d = %d\n", 2, 3, 6);
+        System.out.printf("%d * %d = %d\n", 2, 4, 8);
+        System.out.printf("%d * %d = %d\n", 2, 5, 10);
+        System.out.printf("%d * %d = %d\n", 2, 6, 12);
+        System.out.printf("%d * %d = %d\n", 2, 7, 14);
+        System.out.printf("%d * %d = %d\n", 2, 8, 16);
+        System.out.printf("%d * %d = %d\n", 2, 9, 18);
     }
 }
