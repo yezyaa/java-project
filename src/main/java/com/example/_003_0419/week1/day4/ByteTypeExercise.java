3d109a67f29a00f3f6230aede700871fca4280e0
yezyaa <iyeahji@gmail.com>
2023-04-20 14:48:19 +0900

    feat: Byte 범위 확인 - ByteTypeExercise

---
+++ src/main/java/com/example/_003_0419/week1/day4/ByteTypeExercise.java
@@ -0,0 +1,8 @@
+package com.example._003_0419.week1.day4;
+
+public class ByteTypeExercise {
+    public static void main(String[] args) {
+        byte byte1 = 10; // -128 ~ 127 까지 담김
+        System.out.println(byte1);
+    }
+}
