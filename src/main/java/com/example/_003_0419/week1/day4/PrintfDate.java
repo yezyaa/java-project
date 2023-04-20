06e7813a4477ad1cc5b0aa255fe3307f8b8cca00
yezyaa <iyeahji@gmail.com>
2023-04-20 11:48:59 +0900

    feat: printf 사용해서 날짜 형식 맞춰서 출력하기 - PrintfDate 추가

---
+++ src/main/java/com/example/_003_0419/week1/day4/PrintfDate.java
@@ -0,0 +1,11 @@
+package com.example._003_0419.week1.day4;
+
+public class PrintfDate {
+    public static void main(String[] args) {
+        System.out.printf("%d\n", 4);
+        System.out.printf("%02d\n", 4);
+        System.out.printf("%02d\n", 11);
+        // 2023-04-20
+        System.out.printf("%d-%02d-%02d\n", 2023, 4, 20);
+    }
+}
