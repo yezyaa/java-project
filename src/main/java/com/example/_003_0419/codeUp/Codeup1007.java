13fd5c9019e01009b0cdf48138e171a55ca86172
yezyaa <iyeahji@gmail.com>
2023-04-20 10:21:16 +0900

    feat: CodeUp 1006~1008 예제풀기

---
+++ src/main/java/com/example/_003_0419/codeUp/Codeup1007.java
@@ -0,0 +1,7 @@
+package com.example._003_0419.codeUp;
+
+public class Codeup1007 {
+    public static void main(String[] args) {
+        System.out.println("\"C:\\Download\\hello.cpp\"");
+    }
+}
