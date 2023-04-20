13a5d5def2954389ca789797104b8bf0734cff0b
yezyaa <iyeahji@gmail.com>
2023-04-20 15:43:09 +0900

    feat: CodeUp 1014 예제풀기

---
+++ src/main/java/com/example/_003_0419/codeUp/Codeup1014.java
@@ -0,0 +1,12 @@
+package com.example._003_0419.codeUp;
+
+import java.util.Scanner;
+
+public class Codeup1014 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String val1 = sc.next();
+        String val2 = sc.next();
+        System.out.println(val2 + " " + val1);
+    }
+}
