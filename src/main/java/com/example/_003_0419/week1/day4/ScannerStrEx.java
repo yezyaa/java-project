1ab36a931165c7d494bc91fc23c7bf2a94d83918
yezyaa <iyeahji@gmail.com>
2023-04-20 10:04:39 +0900

    feat: Scanner 사용해서 문자열 두 개 합치기 - ScannerStrEx, ScannerStrExTest 추가

---
+++ src/main/java/com/example/_003_0419/week1/day4/ScannerStrEx.java
@@ -0,0 +1,10 @@
+package com.example._003_0419.week1.day4;
+
+import java.util.Scanner;
+
+public class ScannerStrEx {
+    void inputStringsAndPlus() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.next() + sc.next());
+    }
+}
---
+++ src/main/java/com/example/_003_0419/week1/day4/ScannerStrExTest.java
@@ -0,0 +1,8 @@
+package com.example._003_0419.week1.day4;
+
+public class ScannerStrExTest {
+    public static void main(String[] args) {
+        ScannerStrEx scannerStrEx = new ScannerStrEx();
+        scannerStrEx.inputStringsAndPlus();
+    }
+}
