19c5bec5ee06f28ff608ac6e9020a9bfe12d1b67
yezyaa <iyeahji@gmail.com>
2023-04-20 10:37:05 +0900

    feat: 입력받을 때 안내문 출력하기 - ScannerExAdd 수정

--- src/main/java/com/example/_003_0419/week1/day4/ScannerExAdd.java
+++ src/main/java/com/example/_003_0419/week1/day4/ScannerExAdd.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 public class ScannerExAdd {
     public void readTwoNumbersAndPlus() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println(scanner.nextInt() + scanner.nextInt());
+        System.out.println("두 개의 숫자를 입력하세요. 입력하고 엔터 입력하고 엔터");
+        System.out.print("첫 번째 숫자 : ");
+        System.out.println("첫 번째 숫자는 " + scanner.nextInt() + "입니다");
+        System.out.print("두 번째 숫자 : ");
+        System.out.println("두 번째 숫자는 " + scanner.nextInt() + "입니다");
     }
 }
