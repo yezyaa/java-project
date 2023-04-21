dd31428370e7540ecb55797893deeaac47b49f64
yezyaa <iyeahji@gmail.com>
2023-04-21 11:11:55 +0900

    feat: CodeUp 1018 예제풀기

---
+++ src/main/java/com/example/_003_0419/codeUp/Codeup1018.java
@@ -0,0 +1,11 @@
+package com.example._003_0419.codeUp;
+
+import java.util.Scanner;
+
+public class Codeup1018 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String time = sc.next();
+        System.out.printf(time);
+    }
+}
