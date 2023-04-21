c45e40c83e6ab27bce4f8294dbca0db7f4cf5ace
yezyaa <iyeahji@gmail.com>
2023-04-21 14:15:23 +0900

    feat: 멤버 변수 값 출력 - SpaceInvadersMemberVariable 수정

--- src/main/java/com/example/_003_0419/week1/day5/SpaceInvadersMemberVariable.java
+++ src/main/java/com/example/_003_0419/week1/day5/SpaceInvadersMemberVariable.java
@@ -1,15 +1,19 @@
 package com.example._003_0419.week1.day5;
 
 public class SpaceInvadersMemberVariable {
-    int location; // 멤버변수 (클래스의 멤버라는 뜻)
-    
+    int location; // 멤버변수
+
     public void moveLeft() {
+        location = location - 1;
+    }
+
+    public void moveLeftAndPrint() {
         location = location - 1;
         System.out.printf("moveLeft: %d\n", location);
     }
+
     public void moveRight() {
         location = location + 1;
-        System.out.printf("moveRight: %d\n", location);
     }
 
     public static void main(String[] args) {
@@ -19,5 +23,10 @@ public class SpaceInvadersMemberVariable {
         simv.moveRight();
         simv.moveRight();
         simv.moveRight();
+
+        System.out.printf("최종 위치: %d\n", simv.location);
+
+        simv.moveLeft();
+        System.out.printf("최종 위치: %d\n", simv.location);
     }
 }
