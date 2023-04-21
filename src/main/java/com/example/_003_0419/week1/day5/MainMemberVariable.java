6b941dbfe3739143e84dca984b713c2d97cf2233
yezyaa <iyeahji@gmail.com>
2023-04-21 14:13:41 +0900

    feat: 클래스의 멤버 변수 (static) - MainMemberVariable 추가

---
+++ src/main/java/com/example/_003_0419/week1/day5/MainMemberVariable.java
@@ -0,0 +1,9 @@
+package com.example._003_0419.week1.day5;
+
+public class MainMemberVariable {
+    static int location;
+
+    public static void main(String[] args) {
+        System.out.println(location);
+    }
+}
