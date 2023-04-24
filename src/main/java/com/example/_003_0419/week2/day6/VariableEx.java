6ced853ed9cbb230d918991cf0e95bc7ca50942f
yezyaa <iyeahji@gmail.com>
2023-04-24 10:04:41 +0900

    feat: 원시 타입과 참조 타입 2 - VariableEx 추가

---
+++ src/main/java/com/example/_003_0419/week2/day6/VariableEx.java
@@ -0,0 +1,12 @@
+package com.example._003_0419.week2.day6;
+
+public class VariableEx {
+    public static void main(String[] args) {
+        // 모두 다 1이지만 타입이 각각 다름
+        String sOne = new String("1");
+        int iOne = 1;
+        float fOne = 1.0f;
+
+        System.out.printf("String:%s Number:%d Float:%f", sOne, iOne, fOne);
+    }
+}
