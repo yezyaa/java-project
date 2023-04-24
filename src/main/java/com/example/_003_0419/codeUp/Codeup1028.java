64361015dfe6efd1dc782412b49321bd73432045
yezyaa <iyeahji@gmail.com>
2023-04-24 15:39:51 +0900

    feat: CodeUp 1021~1023, 1028~1030 예제풀기

---
+++ src/main/java/com/example/_003_0419/codeUp/Codeup1028.java
@@ -0,0 +1,12 @@
+package com.example._003_0419.codeUp;
+
+import java.util.Scanner;
+
+public class Codeup1028 {
+    public static void main(String[] args) {
+        long val;
+        Scanner sc = new Scanner(System.in);
+        val = sc.nextLong();
+        System.out.println(val);
+    }
+}
---
+++ src/main/java/com/example/_003_0419/codeUp/Codeup1030.java
@@ -0,0 +1,10 @@
+package com.example._003_0419.codeUp;
+
+import java.util.Scanner;
+
+public class Codeup1030 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("%d", sc.nextBigInteger());
+    }
+}
