7dcb185e47fc742fa08a13c7c1c0dd47d482a4a6
yezyaa <iyeahji@gmail.com>
2023-04-24 13:25:24 +0900

    feat: 데이터 타입을 변환하는 타입 캐스팅 - StringPlusInt 추가

---
+++ src/main/java/com/example/_003_0419/week2/day6/StringPlusInt.java
@@ -0,0 +1,13 @@
+package com.example._003_0419.week2.day6;
+
+public class StringPlusInt {
+    public static void main(String[] args) {
+        System.out.println("1" + 1);
+        //int result = 1 + "1";
+        String result = 1 + "1";
+        System.out.println(result);
+
+        int iResult = 1 + Integer.parseInt("1");
+        System.out.println(iResult);
+    }
+}
