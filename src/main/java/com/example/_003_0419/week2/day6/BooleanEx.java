a6e424c28c301e5115cca1332cf0e1047d838f60
yezyaa <iyeahji@gmail.com>
2023-04-24 14:38:15 +0900

    feat: true와 false를 저장하는 boolean 타입 - BooleanEx 추가

---
+++ src/main/java/com/example/_003_0419/week2/day6/BooleanEx.java
@@ -0,0 +1,13 @@
+package com.example._003_0419.week2.day6;
+
+public class BooleanEx {
+    public static void main(String[] args) {
+        boolean flag1 = false;
+        boolean flag2 = true;
+
+        boolean isPaymentSuccess = true;
+        boolean isBalanceSufficient = false;
+
+        System.out.println(isPaymentSuccess == isBalanceSufficient);
+    }
+}
