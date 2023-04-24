4ef76cc4fedf28c3ba7b83bd63618b1e7fd63674
yezyaa <iyeahji@gmail.com>
2023-04-24 10:04:13 +0900

    feat: 원시 타입과 참조 타입 - DefineAVariable 추가

---
+++ src/main/java/com/example/_003_0419/week2/day6/DefineAVariable.java
@@ -0,0 +1,9 @@
+package com.example._003_0419.week2.day6;
+
+public class DefineAVariable {
+    public static void main(String[] args) {
+        int num = 10;
+        String name = "이예지"; // primitive 타입 쓰듯이 초기화
+        String name2 = new String("이예지"); // 원래는 이렇게 생김 / ()안에 들어가는 것이 파라미터라고 함
+    }
+}
