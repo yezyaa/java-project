3604dd5d58617cd60a8df10d69a96c1d1fcd0252
yezyaa <iyeahji@gmail.com>
2023-04-25 14:19:22 +0900

    feat: 배열 시작 - StudentMathScore 추가

---
+++ src/main/java/com/example/_003_0419/week2/day7/StudentMathScore.java
@@ -0,0 +1,12 @@
+package com.example._003_0419.week2.day7;
+
+public class StudentMathScore {
+    public static void main(String[] args) {
+        int student1MathScore = 100;
+        int student2MathScore = 100;
+        int student3MathScore = 100;
+        int student4MathScore = 100;
+        int student5MathScore = 100;
+        int student6MathScore = 100;
+    }
+}
