bd6b326efdfd8477b735f4084a0a9941f77c8c1e
yezyaa <iyeahji@gmail.com>
2023-04-25 10:47:52 +0900

    feat: 문자열 연결 - StringConcat 추가

---
+++ src/main/java/com/example/_003_0419/week2/day7/StringConcat.java
@@ -0,0 +1,10 @@
+package com.example._003_0419.week2.day7;
+
+public class StringConcat {
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "World";
+        String result = str1 + str2;
+        System.out.println(result);
+    }
+}
