f684fc6f8b1221d2519a58b82c8a67bab25fa8be
yezyaa <iyeahji@gmail.com>
2023-04-25 14:03:31 +0900

    feat: CodeUp 1018,1019 예제풀기

--- src/main/java/com/example/_003_0419/codeUp/Codeup1018.java
+++ src/main/java/com/example/_003_0419/codeUp/Codeup1018.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Codeup1018 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String time = sc.next();
-        System.out.printf(time);
+        System.out.println(sc.nextLine());
     }
 }
