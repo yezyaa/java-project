28702a2a820e032f54a3e1ec1d23befc5385e44a
yezyaa <iyeahji@gmail.com>
2023-04-25 11:04:12 +0900

    feat: 문자열과 변수 연결 - AccountMessage 추가

---
+++ src/main/java/com/example/_003_0419/week2/day7/AccountMessage.java
@@ -0,0 +1,10 @@
+package com.example._003_0419.week2.day7;
+
+public class AccountMessage {
+    public static void main(String[] args) {
+        String name = "이예지";
+        int balance = 20_000_000;
+        String message = name + "님의 통장 잔고는 " + balance + "원 입니다.";
+        System.out.println(message);
+    }
+}
