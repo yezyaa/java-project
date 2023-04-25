7f134b4e0ace3d339c8ac9dd675d7ae484fd6e69
yezyaa <iyeahji@gmail.com>
2023-04-25 11:16:35 +0900

    feat: CodeUp 1020 예제풀기

---
+++ src/main/java/com/example/_003_0419/week2/day8/StringSub.java
@@ -0,0 +1,10 @@
+package com.example._003_0419.week2.day8;
+
+public class StringSub {
+    public static void main(String[] args) {
+        String str = "Hello";
+        System.out.println(str.substring(0, 1)); // H
+        System.out.println(str.substring(0, 2)); // He
+        System.out.println(str.substring(1, 3)); // el
+    }
+}
