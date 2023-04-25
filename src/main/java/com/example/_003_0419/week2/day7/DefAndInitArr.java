9a704c87ec903822023e3991f6143c9e0d462cc6
yezyaa <iyeahji@gmail.com>
2023-04-25 14:20:01 +0900

    feat: 배열을 선언하면서 초기화하기 - DefAndInitArr 추가

---
+++ src/main/java/com/example/_003_0419/week2/day7/DefAndInitArr.java
@@ -0,0 +1,8 @@
+package com.example._003_0419.week2.day7;
+
+public class DefAndInitArr {
+    public static void main(String[] args) {
+        int[] iArr = new int[5]; // 배열 초기화 할 때는 반드시 개수 넣어주기.
+        System.out.println(iArr);
+    }
+}
