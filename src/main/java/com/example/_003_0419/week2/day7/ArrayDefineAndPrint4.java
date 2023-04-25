e50c9f4165b0534bf226cb3c0b1cccedab1cee89
yezyaa <iyeahji@gmail.com>
2023-04-25 14:28:57 +0900

    feat: 배열에 값 넣기 - ArrayDefineAndPrint4 추가

---
+++ src/main/java/com/example/_003_0419/week2/day7/ArrayDefineAndPrint4.java
@@ -0,0 +1,10 @@
+package com.example._003_0419.week2.day7;
+
+import java.util.Arrays;
+
+public class ArrayDefineAndPrint4 {
+    public static void main(String[] args) {
+        int[] iArr = new int[]{1, 2, 3, 8};
+        System.out.println(Arrays.toString(iArr));
+    }
+}
