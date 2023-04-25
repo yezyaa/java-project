a1327776b9785373e667a7b0917179804f279e23
yezyaa <iyeahji@gmail.com>
2023-04-25 10:48:35 +0900

    feat: 문자열 연결 + 공백 추가 - StringConcat 수정

--- src/main/java/com/example/_003_0419/week2/day7/StringConcat.java
+++ src/main/java/com/example/_003_0419/week2/day7/StringConcat.java
@@ -4,7 +4,7 @@ public class StringConcat {
     public static void main(String[] args) {
         String str1 = "Hello";
         String str2 = "World";
-        String result = str1 + str2;
+        String result = str1 + " " + str2;
         System.out.println(result);
     }
 }
