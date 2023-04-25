ae1dd0fde443267b5bb7c739e11bfc5a8fb35135
yezyaa <iyeahji@gmail.com>
2023-04-25 14:51:27 +0900

    feat: 여러가지 타입의 배열 선언 - ReferenceTypeArray 추가

---
+++ src/main/java/com/example/_003_0419/week2/day7/ReferenceTypeArray.java
@@ -0,0 +1,12 @@
+package com.example._003_0419.week2.day7;
+
+import java.util.Arrays;
+public class ReferenceTypeArray {
+    public static void main(String[] args) {
+        String[] sArr = new String[3];
+        float[] fArr = new float[3];
+
+        System.out.println(Arrays.toString(sArr));
+        System.out.println(Arrays.toString(fArr));
+    }
+}
