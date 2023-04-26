6c1e4f7f4b5be31f111360df4672f23e3dae54b4
yezyaa <iyeahji@gmail.com>
2023-04-26 13:17:34 +0900

    feat: 가로 한 줄에 값 채우기 + 행 번호를 넣으면 그 행을 1로 값을 넣어주는 static method - TwoDimArrFillRow 수정

--- src/main/java/com/example/_003_0419/week2/day8/TwoDimArrFillRow.java
+++ src/main/java/com/example/_003_0419/week2/day8/TwoDimArrFillRow.java
@@ -2,20 +2,29 @@ package com.example._003_0419.week2.day8;
 
 import java.util.Arrays;
 
-public class TwoDimArrFillRow {
-    public static void main(String[] args) {
-        int[][] arr = new int[5][5];
+    public class TwoDimArrFillRow {
+    // 행 번호를 넣으면 그 행을 1로 값을 넣어주는 static method
+    public static void fillARow(int[][] arr, int rowNum) {
+        arr[rowNum][0] = 1;
+        arr[rowNum][1] = 1;
+        arr[rowNum][2] = 1;
+        arr[rowNum][3] = 1;
+        arr[rowNum][4] = 1;
+        printTwoDimArr(arr);
+    }
 
-        arr[2][0] = 1;
-        arr[2][1] = 1;
-        arr[2][2] = 1;
-        arr[2][3] = 1;
-        arr[2][4] = 1;
-
+    public static void printTwoDimArr(int[][] arr) {
         System.out.println(Arrays.toString(arr[0]));
         System.out.println(Arrays.toString(arr[1]));
         System.out.println(Arrays.toString(arr[2]));
         System.out.println(Arrays.toString(arr[3]));
         System.out.println(Arrays.toString(arr[4]));
+        System.out.println("---------------");
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = new int[5][5];
+        fillARow(arr, 1);
+        fillARow(arr, 2);
     }
 }
