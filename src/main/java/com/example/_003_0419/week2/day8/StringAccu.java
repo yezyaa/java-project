5e0c4dd8b488c2ab888c774d5218ac2d982e62a6
yezyaa <iyeahji@gmail.com>
2023-04-26 15:43:00 +0900

    feat: 문자열 누적하기 - StringAccu 추가

---
+++ src/main/java/com/example/_003_0419/week2/day8/StringAccu.java
@@ -0,0 +1,11 @@
+package com.example._003_0419.week2.day8;
+
+public class StringAccu {
+    public static void main(String[] args) {
+        String answer = "";
+        answer = answer + "쿵";
+        System.out.println(answer);
+        answer = answer + "짝";
+        System.out.println(answer);
+    }
+}
