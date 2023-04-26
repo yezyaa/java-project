ad55089dc9a403a0047843e41a55ba5f5db4194c
yezyaa <iyeahji@gmail.com>
2023-04-26 11:15:35 +0900

    feat: 2차원 배열의 보기 좋은 형태 나열 - DefineArr2 추가

---
+++ src/main/java/com/example/_003_0419/week2/day8/DefineArr2.java
@@ -0,0 +1,28 @@
+package com.example._003_0419.week2.day8;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class DefineArr2 {
+    public static void main(String[] args) {
+        int[] iArr = new int[]{1, 2, 3, 8};
+        System.out.println("iArr[0]" + iArr[0]);
+
+        int[][] arr32 = new int[3][2];
+        int[][] arr67 = new int[6][7];
+
+        // 3 * 3
+        int[][] arr2 = {
+                {10,20,30},
+                {40,50,60},
+                {70,80,90}
+        };
+
+        System.out.println(Arrays.toString(arr2[0]));
+        System.out.println(Arrays.toString(arr2[1]));
+        System.out.println(Arrays.toString(arr2[2]));
+
+        System.out.println(arr2[0][0]);
+        System.out.println(arr2[0][1]);
+    }
+}
