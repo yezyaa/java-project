86e28223e39d5257fea1d9e925865e8229642037
yezyaa <iyeahji@gmail.com>
2023-04-26 10:01:44 +0900

    feat: 배열을 멤버 변수로 - ClassInSchool, Teacher, ClassInSchoolTest 추가

---
+++ src/main/java/com/example/_003_0419/week2/day8/ClassInSchool.java
@@ -0,0 +1,7 @@
+package com.example._003_0419.week2.day8;
+
+public class ClassInSchool {
+    int no; // 1반~10반
+    Teacher teacher;
+    Student[] students; // Student는 클래스지만 또 다른 클래스의 멤버변수가 될 수 있다.
+}
---
+++ src/main/java/com/example/_003_0419/week2/day8/Teacher.java
@@ -0,0 +1,8 @@
+package com.example._003_0419.week2.day8;
+
+public class Teacher {
+    long id;
+    String name;
+    int age;
+    String address;
+}
