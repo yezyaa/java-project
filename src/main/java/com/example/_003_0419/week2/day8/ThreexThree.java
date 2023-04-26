77b131335443df2de4d389274c6c72cbcb281b7f
yezyaa <iyeahji@gmail.com>
2023-04-26 10:32:31 +0900

    feat: 2차원 배열을 선언하는 방법 - ThreexThree, TenxTen 추가

---
+++ src/main/java/com/example/_003_0419/week2/day8/ThreexThree.java
@@ -0,0 +1,12 @@
+package com.example._003_0419.week2.day8;
+
+import java.util.Arrays;
+
+public class ThreexThree {
+    public static void main(String[] args) {
+        int[][] arr = new int[3][3];
+        System.out.println(Arrays.toString(arr[0]));
+        System.out.println(Arrays.toString(arr[1]));
+        System.out.println(Arrays.toString(arr[2]));
+    }
+}
