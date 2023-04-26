564f9b65496abedce02952f2219a76efe7cfe9ba
yezyaa <iyeahji@gmail.com>
2023-04-26 13:37:41 +0900

    feat: 배열의 길이 - ArrLength 추가

---
+++ src/main/java/com/example/_003_0419/week2/day8/ArrLength.java
@@ -0,0 +1,8 @@
+package com.example._003_0419.week2.day8;
+
+public class ArrLength {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        System.out.println(arr.length);
+    }
+}
