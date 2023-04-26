a7f5f0d89a28479b56ab327e76506c17fd3a167c
yezyaa <iyeahji@gmail.com>
2023-04-26 11:36:29 +0900

    feat: 2차원 배열 값에 접근하기 + 메소드로 Refactoring - TwoDimArray 수정

--- src/main/java/com/example/_003_0419/week2/day8/TwoDimArray.java
+++ src/main/java/com/example/_003_0419/week2/day8/TwoDimArray.java
@@ -3,13 +3,25 @@ package com.example._003_0419.week2.day8;
 import java.util.Arrays;
 
 public class TwoDimArray {
-    public static void main(String[] args) {
-        int[][] arr = new int[3][3]; // 2차원 배열 선언
-        int[] arr0 = arr[0]; // 2차원 배열에서 배열을 꺼내 1차원 배열이 됨
-        arr0[0] = 1; // 1차원 배열인 arr0에서 첫번째 칸에 값을 할당
-
+    // 메소드(method, 함수, function)로 Refactoring
+    // parameter
+    public static void printArray(int[][] arr) {
         System.out.println(Arrays.toString(arr[0]));
         System.out.println(Arrays.toString(arr[1]));
         System.out.println(Arrays.toString(arr[2]));
+        System.out.println("---------");
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = new int[3][3]; // 2차원 배열 선언
+        printArray(arr);
+
+        arr[0][0] = 1;
+        arr[0][2] = 2;
+        arr[1][0] = 3;
+        printArray(arr);
+
+        arr[1][1] = 5;
+        printArray(arr);
     }
 }
