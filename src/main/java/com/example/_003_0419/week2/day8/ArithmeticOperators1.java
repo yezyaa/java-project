ebb11528eacf66104c3e3307ce51a518b79c94d1
yezyaa <iyeahji@gmail.com>
2023-04-26 14:02:13 +0900

    feat: 사칙연산과 %연산 결과를 한 번에 출력하기 - ArithmeticOperators1 추가

---
+++ src/main/java/com/example/_003_0419/week2/day8/ArithmeticOperators1.java
@@ -0,0 +1,13 @@
+package com.example._003_0419.week2.day8;
+
+public class ArithmeticOperators1 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+        System.out.printf("a + b = %d\n", a + b);
+        System.out.printf("a - b = %d\n", a - b);
+        System.out.printf("a * b = %d\n", a * b);
+        System.out.printf("a / b = %d\n", a / b);
+        System.out.printf("a %% b = %d\n", a % b);
+    }
+}
