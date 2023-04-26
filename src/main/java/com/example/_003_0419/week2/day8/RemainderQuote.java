fb57b700d1cbb54152354d7d0cb7f5fc77fcdfe2
yezyaa <iyeahji@gmail.com>
2023-04-26 14:05:37 +0900

    feat: 몫과 나머지 - RemainderQuote 추가

---
+++ src/main/java/com/example/_003_0419/week2/day8/RemainderQuote.java
@@ -0,0 +1,11 @@
+package com.example._003_0419.week2.day8;
+
+public class RemainderQuote {
+    public static void main(String[] args) {
+        int iVal = 1;
+        System.out.println(iVal / 2);
+        
+        // 몫
+        // 나머지
+    }
+}
