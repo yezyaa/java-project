e6485bb651d75c6f723242318adef2e44820d6f8
yezyaa <iyeahji@gmail.com>
2023-04-26 13:22:24 +0900

    feat: 배열 인덱스간에 값 바꾸기 - SwapEx 추가

---
+++ src/main/java/com/example/_003_0419/week2/day8/SwapEx.java
@@ -0,0 +1,15 @@
+package com.example._003_0419.week2.day8;
+
+import java.util.Arrays;
+
+public class SwapEx {
+    public static void main(String[] args) {
+        // 오름차순으로 정렬하는 알고리즘을 만들어 보세요.
+        int[] arr = {2, 1, 4, 8, 7, 6};
+        int temp = arr[0];
+        arr[0] = arr[1];
+        arr[1] = arr[0];
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
