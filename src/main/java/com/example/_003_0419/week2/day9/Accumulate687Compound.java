2b3de74018c415907adea397b4df1fa8ceca0420
yezyaa <iyeahji@gmail.com>
2023-04-27 09:56:31 +0900

    feat: 687의 각 자릿수를 더하는 코드에 적용 - Accumulate687Compound 추가

---
+++ src/main/java/com/example/_003_0419/week2/day9/Accumulate687Compound.java
@@ -0,0 +1,17 @@
+package com.example._003_0419.week2.day9;
+
+public class Accumulate687Compound {
+    public static void main(String[] args) {
+        int num = 687;
+        int answer = 0;
+
+        // 1 나머지 2 몫
+        answer += num % 10;
+        num /= 10;
+        answer += num % 10;
+        num /= 10;
+        answer += num % 10;
+        num /= 10;
+        System.out.println(answer);
+    }
+}
