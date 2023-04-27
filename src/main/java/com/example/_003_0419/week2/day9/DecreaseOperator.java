e50a6680d1cac8314983adc4436d60e193c747e0
yezyaa <iyeahji@gmail.com>
2023-04-27 10:12:05 +0900

    feat: 증감연산자, 가감연산자 - IncreaseOperator, DecreaseOperator 추가

---
+++ src/main/java/com/example/_003_0419/week2/day9/DecreaseOperator.java
@@ -0,0 +1,14 @@
+package com.example._003_0419.week2.day9;
+
+public class DecreaseOperator {
+    public static void main(String[] args) {
+        int i = 10;
+        System.out.println("[--가 뒤에 오는 경우]");
+        System.out.printf("i: %d\n", i);
+        System.out.printf("i--: %d\n", i--);
+        System.out.printf("i: %d\n\n", i);
+        System.out.println("[--가 앞에 오는 경우]");
+        System.out.printf("i: %d\n", i);
+        System.out.printf("--i: %d\n", --i);
+    }
+}
