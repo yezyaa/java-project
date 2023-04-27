a4b47f2439a27901fd52907c128b4d569309b32b
yezyaa <iyeahji@gmail.com>
2023-04-27 10:19:24 +0900

    feat: 증감연산자를 활용한 배열 연산 - IncreaseFirst, IncreaseFirst2 추가

---
+++ src/main/java/com/example/_003_0419/week2/day9/IncreaseFirst.java
@@ -0,0 +1,13 @@
+package com.example._003_0419.week2.day9;
+
+public class IncreaseFirst {
+    public static void main(String[] args) {
+        int idx = 0;
+        int[] arr = {2, 1, 7, 4};
+        System.out.println(arr[idx]);
+        idx += 1;
+        System.out.println(arr[idx]);
+        idx += 1;
+        System.out.println(arr[idx]);
+    }
+}
---
+++ src/main/java/com/example/_003_0419/week2/day9/IncreaseFirst2.java
@@ -0,0 +1,11 @@
+package com.example._003_0419.week2.day9;
+
+public class IncreaseFirst2 {
+    public static void main(String[] args) {
+        int idx = 0;
+        int[] arr = {2, 1, 7, 4};
+        System.out.println(arr[idx++]);
+        System.out.println(arr[idx++]);
+        System.out.println(arr[idx++]);
+    }
+}
