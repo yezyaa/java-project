33a5b4bcdbc3b913c453025512c5cc00cd2fb1cd
yezyaa <iyeahji@gmail.com>
2023-04-27 15:11:02 +0900

    feat: CodeUp 1059~1062 예제풀기

---
+++ src/main/java/com/example/_003_0419/codeUp/Codeup1060.java
@@ -0,0 +1,10 @@
+package com.example._003_0419.codeUp;
+
+import java.util.Scanner;
+
+public class Codeup1060 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt() & sc.nextInt());
+    }
+}
