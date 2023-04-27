32511ce6cea57e43847547a1c861478d19625360
yezyaa <iyeahji@gmail.com>
2023-04-27 14:18:33 +0900

    feat: CodeUp 1053~1058 예제풀기

---
+++ src/main/java/com/example/_003_0419/codeUp/Codeup1056.java
@@ -0,0 +1,14 @@
+package com.example._003_0419.codeUp;
+
+import java.util.Scanner;
+
+public class Codeup1056 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int val1 = sc.nextInt();
+        int val2 = sc.nextInt();
+
+        boolean result = val1 != val2;
+        System.out.printf("%d", result ? 1 : 0);
+    }
+}
