ac9507415446f3b30977d0e62ed82e9a6d5b3b2b
yezyaa <iyeahji@gmail.com>
2023-04-27 14:50:25 +0900

    feat: 비트연산자2 - BitOperatorEx2 추가

---
+++ src/main/java/com/example/_003_0419/week2/day9/BitOperatorEx2.java
@@ -0,0 +1,10 @@
+package com.example._003_0419.week2.day9;
+
+public class BitOperatorEx2 {
+    public static void main(String[] args) {
+        int val1 = 1;
+        System.out.println(~val1);
+        System.out.println(Integer.toBinaryString(val1));
+        System.out.println(Integer.toBinaryString(~val1));
+    }
+}
