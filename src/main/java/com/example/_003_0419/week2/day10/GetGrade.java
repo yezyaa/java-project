d312ef21fc6061423801a486be8b3943ba6c392a
yezyaa <iyeahji@gmail.com>
2023-04-28 10:19:10 +0900

    feat: else if의 사용 - IfWrong, GetGrade 추가

---
+++ src/main/java/com/example/_003_0419/week2/day10/GetGrade.java
@@ -0,0 +1,16 @@
+package com.example._003_0419.week2.day10;
+
+public class GetGrade {
+    public static void main(String[] args) {
+        int score = 75;
+        if (score >= 90) {
+            System.out.println("A");
+        } else if (score >= 80) {
+            System.out.println("B");
+        } else if (score >= 70) {
+            System.out.println("C");
+        } else {
+            System.out.println("F");
+        }
+    }
+}
---
+++ src/main/java/com/example/_003_0419/week2/day10/IfWrong.java
@@ -0,0 +1,15 @@
+package com.example._003_0419.week2.day10;
+
+public class IfWrong {
+    public static void main(String[] args) {
+        int score = 81;
+
+        if (score >= 90) {
+            System.out.println("A");
+        } if (score >= 80) {
+            System.out.println("B");
+        } if (score >= 70) {
+            System.out.println("C");
+        }
+    }
+}
