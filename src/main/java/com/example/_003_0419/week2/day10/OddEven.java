368f511f720e2b99c1d54eb022870872909313a3
yezyaa <iyeahji@gmail.com>
2023-04-28 09:33:29 +0900

    feat: 홀수인지 짝수인지 판단하기 - OddEven 추가

---
+++ src/main/java/com/example/_003_0419/week2/day10/OddEven.java
@@ -0,0 +1,12 @@
+package com.example._003_0419.week2.day10;
+
+public class OddEven {
+    public static void main(String[] args) {
+        int num = 1;
+        if (num % 2 == 0) {
+            System.out.println("짝수입니다.");
+        } else {
+            System.out.println("홀수입니다.");
+        }
+    }
+}
