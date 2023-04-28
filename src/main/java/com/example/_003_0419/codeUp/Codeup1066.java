87858cdee9b3cf3661e3dabc511020df4f842182
yezyaa <iyeahji@gmail.com>
2023-04-28 09:42:12 +0900

    feat: CodeUp 1065~1067 예제풀기

---
+++ src/main/java/com/example/_003_0419/codeUp/Codeup1066.java
@@ -0,0 +1,28 @@
+package com.example._003_0419.codeUp;
+
+import java.util.Scanner;
+
+public class Codeup1066 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int val1 = sc.nextInt();
+        int val2 = sc.nextInt();
+        int val3 = sc.nextInt();
+
+        if (val1 % 2 == 0) {
+            System.out.println("even");;
+        } else {
+            System.out.println("odd");
+        }
+        if (val2 % 2 == 0) {
+            System.out.println("even");;
+        } else {
+            System.out.println("odd");
+        }
+        if (val3 % 2 == 0) {
+            System.out.println("even");;
+        } else {
+            System.out.println("odd");
+        }
+    }
+}
