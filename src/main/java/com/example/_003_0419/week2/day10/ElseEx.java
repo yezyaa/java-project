4f8b75024d5a681df7afacf1acaed2f6cee53d0b
yezyaa <iyeahji@gmail.com>
2023-04-28 09:17:31 +0900

    feat: else의 사용 - ElseEx, ElseEx2 추가

---
+++ src/main/java/com/example/_003_0419/week2/day10/ElseEx.java
@@ -0,0 +1,19 @@
+package com.example._003_0419.week2.day10;
+
+public class ElseEx {
+    public static void main(String[] args) {
+        boolean checkTrue = true;
+        if (checkTrue) {
+            System.out.println("true입니다.");
+        } else {
+            System.out.println("false입니다.");
+        }
+
+        boolean checkFalse = false;
+        if (checkFalse) {
+            System.out.println("true입니다.");
+        } else {
+            System.out.println("false입니다.");
+        }
+    }
+}
---
+++ src/main/java/com/example/_003_0419/week2/day10/ElseEx2.java
@@ -0,0 +1,12 @@
+package com.example._003_0419.week2.day10;
+
+public class ElseEx2 {
+    public static void main(String[] args) {
+        int account = 1400;
+        if (account >= 1500) {
+            System.out.println("잔액을 차감합니다.");
+        } else {
+            System.out.println("'잔액이 부족합니다.'를 재생합니다.");
+        }
+    }
+}
