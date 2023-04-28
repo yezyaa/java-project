eae7fb6adf76f30d0f9f60e4b6e59174227ac4d3
yezyaa <iyeahji@gmail.com>
2023-04-28 09:07:31 +0900

    feat: if문의 사용 방법 - IfEx 추가

---
+++ src/main/java/com/example/_003_0419/week2/day10/IfEx.java
@@ -0,0 +1,12 @@
+package com.example._003_0419.week2.day10;
+
+public class IfEx {
+    public static void main(String[] args) {
+        if (true) {
+            System.out.println("항상 실행 됩니다.");
+        }
+        if (false) {
+            System.out.println("항상 실행 되지 않습니다.");
+        }
+    }
+}
