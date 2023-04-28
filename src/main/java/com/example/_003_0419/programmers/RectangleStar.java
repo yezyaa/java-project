18356d87f9f8572216b1e9a225b0ed11da33f930
yezyaa <iyeahji@gmail.com>
2023-04-28 13:20:51 +0900

    feat: Programmers 직사각형 별찍기

---
+++ src/main/java/com/example/_003_0419/programmers/RectangleStar.java
@@ -0,0 +1,18 @@
+package com.example._003_0419.programmers;
+
+import java.util.Scanner;
+
+public class RectangleStar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        for (int i = 0; i < b; i++) {
+            for (int j = 0; j < a; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
