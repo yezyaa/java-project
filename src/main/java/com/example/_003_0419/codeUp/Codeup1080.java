3254da8600f992a51d34bf9789071fa425083f99
yezyaa <iyeahji@gmail.com>
2023-05-01 15:19:14 +0900

    feat: CodeUp 1080 예제풀기

---
+++ src/main/java/com/example/_003_0419/codeUp/Codeup1080.java
@@ -0,0 +1,19 @@
+package com.example._003_0419.codeUp;
+
+import java.util.Scanner;
+
+public class Codeup1080 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        int sum = 0;
+
+        for (int i = 1; i <= input; i++) {
+            sum += i;
+            if (input <= sum) {
+                System.out.printf("%d", i);
+                break;
+            }
+        }
+    }
+}
