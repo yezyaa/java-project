31b4dc4014ad004be7a6bf512cc6dffe8e89a0bb
yezyaa <iyeahji@gmail.com>
2023-05-01 15:49:01 +0900

    feat: CodeUp 1081,1082 예제풀기

---
+++ src/main/java/com/example/_003_0419/codeUp/Codeup1081.java
@@ -0,0 +1,17 @@
+package com.example._003_0419.codeUp;
+
+import java.util.Scanner;
+
+public class Codeup1081 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        for (int i = 1; i <= num1; i++) {
+            for (int j = 1; j <= num2; j++) {
+                System.out.printf("%d %d\n", i, j);
+            }
+        }
+    }
+}
