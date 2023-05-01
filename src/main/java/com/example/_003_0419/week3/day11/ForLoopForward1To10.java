83a65ac9c6d1113665c13745fe28dba128287582
yezyaa <iyeahji@gmail.com>
2023-05-01 11:19:57 +0900

    feat: 1부터 10까지 출력하기 - ForLoopForward1To10 추가

---
+++ src/main/java/com/example/_003_0419/week3/day11/ForLoopForward1To10.java
@@ -0,0 +1,9 @@
+package com.example._003_0419.week3.day11;
+
+public class ForLoopForward1To10 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10 ; i++) {
+            System.out.println(i);
+        }
+    }
+}
