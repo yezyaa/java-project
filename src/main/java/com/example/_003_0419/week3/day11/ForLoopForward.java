1c57c97097fd872f4c4e9acd1cd1a0c09b5b27e3
yezyaa <iyeahji@gmail.com>
2023-05-01 10:09:27 +0900

    feat: for 반복문을 사용하여 0~9 정수 출력 - ForLoopForward 추가

---
+++ src/main/java/com/example/_003_0419/week3/day11/ForLoopForward.java
@@ -0,0 +1,9 @@
+package com.example._003_0419.week3.day11;
+
+public class ForLoopForward {
+    public static void main(String[] args) {
+        for (int i = 0; i < 9; i++) {
+            System.out.println(i);
+        }
+    }
+}
