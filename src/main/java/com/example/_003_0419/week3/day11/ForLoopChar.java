9241dcb7c0e7e78a98fdf78580bdc3b86553af8e
yezyaa <iyeahji@gmail.com>
2023-05-01 13:26:10 +0900

    feat: 알파벳 반복하기 + formatting - ForLoopChar 수정

--- src/main/java/com/example/_003_0419/week3/day11/ForLoopChar.java
+++ src/main/java/com/example/_003_0419/week3/day11/ForLoopChar.java
@@ -3,7 +3,8 @@ package com.example._003_0419.week3.day11;
 public class ForLoopChar {
     public static void main(String[] args) {
         for (char c = 'a'; c <= 'z'; c++) {
-            System.out.println(c);
+            //System.out.println(c);
+            System.out.printf("%c ", c);
         }
     }
 }
