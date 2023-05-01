25206d98e5a57857458e52f461a8a4b7492a7c84
yezyaa <iyeahji@gmail.com>
2023-05-01 13:18:06 +0900

    feat: i값을 2씩 늘리기 + 홀수 추가 - ForLoopForwardStep 수정

--- src/main/java/com/example/_003_0419/week3/day11/ForLoopForwardStep.java
+++ src/main/java/com/example/_003_0419/week3/day11/ForLoopForwardStep.java
@@ -5,5 +5,8 @@ public class ForLoopForwardStep {
         for (int i = 0; i < 10; i+=2) {
             System.out.println(i);
         }
+        for (int i = 1; i < 10; i+=2) {
+            System.out.println(i);
+        }
     }
 }
