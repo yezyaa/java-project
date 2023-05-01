125dc226a4e0c4581bdcdcffc3473fdc51b6d3de
yezyaa <iyeahji@gmail.com>
2023-05-01 14:17:53 +0900

    feat: 2단 출력 - MultiplicationTable 추가

---
+++ src/main/java/com/example/_003_0419/codeUp/Codeup1124.java
@@ -0,0 +1,10 @@
+package com.example._003_0419.codeUp;
+
+import java.util.Scanner;
+
+public class Codeup1124 {
+    // C1H1
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/example/_003_0419/week3/day11/MultiplicationTable.java
@@ -0,0 +1,9 @@
+package com.example._003_0419.week3.day11;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 9; i++) {
+            System.out.printf("2 * %d = %d\n", i, 2 * i);
+        }
+    }
+}
