46271009ded5da4d8b99d083428c4dc1ae6e7e9f
yezyaa <iyeahji@gmail.com>
2023-05-01 11:43:40 +0900

    feat: CodeUp 1073~1075 예제풀기

---
+++ src/main/java/com/example/_003_0419/codeUp/Codeup1074.java
@@ -0,0 +1,12 @@
+package com.example._003_0419.codeUp;
+
+import java.util.Scanner;
+
+public class Codeup1074 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for (int i = sc.nextInt(); i > 0; i--) {
+            System.out.printf("%d\n", i);
+        }
+    }
+}
---
+++ src/main/java/com/example/_003_0419/codeUp/Codeup1075.java
@@ -0,0 +1,12 @@
+package com.example._003_0419.codeUp;
+
+import java.util.Scanner;
+
+public class Codeup1075 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for (int i = sc.nextInt(); i > 0; i--) {
+            System.out.printf("%d\n", i - 1);
+        }
+    }
+}
