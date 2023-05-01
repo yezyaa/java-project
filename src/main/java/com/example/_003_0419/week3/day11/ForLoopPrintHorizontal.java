934260e8b5fb127ee26a9c9fd5af2b3650a899c5
yezyaa <iyeahji@gmail.com>
2023-05-01 10:15:29 +0900

    feat: 0~9 숫자 가로로 출력 - ForLoopPrintHorizontal 추가

---
+++ src/main/java/com/example/_003_0419/week3/day11/ForLoopPrintHorizontal.java
@@ -0,0 +1,9 @@
+package com.example._003_0419.week3.day11;
+
+public class ForLoopPrintHorizontal {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("%d ", i);
+        }
+    }
+}
