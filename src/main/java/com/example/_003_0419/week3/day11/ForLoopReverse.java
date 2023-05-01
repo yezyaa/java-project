1b662d73b8c6523e5ab2b6f4b9d108fcdd90fc38
yezyaa <iyeahji@gmail.com>
2023-05-01 11:26:01 +0900

    feat: 5부터 0까지 출력하기 - ForLoopReverse 추가

---
+++ src/main/java/com/example/_003_0419/week3/day11/ForLoopReverse.java
@@ -0,0 +1,10 @@
+package com.example._003_0419.week3.day11;
+
+public class ForLoopReverse {
+    public static void main(String[] args) {
+        for (int i = 5; i >= 0 ; i--) {
+            //System.out.println(i);
+            System.out.printf("%d ", i);
+        }
+    }
+}
