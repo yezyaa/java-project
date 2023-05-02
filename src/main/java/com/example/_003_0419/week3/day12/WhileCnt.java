c00294141e61e85a3ae5ecba46fd4e29d63656e5
yezyaa <iyeahji@gmail.com>
2023-05-02 14:06:07 +0900

    feat: while 카운트 다운 - WhileCnt, WhileCntHori 추가

---
+++ src/main/java/com/example/_003_0419/week3/day12/WhileCnt.java
@@ -0,0 +1,11 @@
+package com.example._003_0419.week3.day12;
+
+public class WhileCnt {
+    public static void main(String[] args) {
+        int cnt = 5;
+        while (cnt < 11) {
+            System.out.println(cnt++);
+        }
+        System.out.println("cnt = " + cnt);
+    }
+}
---
+++ src/main/java/com/example/_003_0419/week3/day12/WhileCntHori.java
@@ -0,0 +1,10 @@
+package com.example._003_0419.week3.day12;
+
+public class WhileCntHori {
+    public static void main(String[] args) {
+        int cnt = 0;
+        while (cnt < 11) {
+            System.out.printf("%d ", cnt++);
+        }
+    }
+}
