f607493a68a3088c1ddf1b3b3c6cfd3ed415270d
yezyaa <iyeahji@gmail.com>
2023-05-02 18:45:08 +0900

    feat: CodeUp 1620 예제풀기

---
+++ src/main/java/com/example/_003_0419/codeUp/Codeup1620.java
@@ -0,0 +1,22 @@
+package com.example._003_0419.codeUp;
+
+import java.util.Scanner;
+
+public class Codeup1620 {
+    public static int sum(int num) {
+        int sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        if (sum >= 10) {
+            sum = sum(sum);
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt(); // 1234567
+        System.out.printf("%d", sum(input));
+    }
+}
