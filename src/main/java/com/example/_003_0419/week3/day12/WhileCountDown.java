f22e5134fd1b1769385b8a6a46b73c4fd7aa8fdb
yezyaa <iyeahji@gmail.com>
2023-05-02 14:26:52 +0900

    feat: while 카운트 다운 - WhileCountDown 추가

---
+++ src/main/java/com/example/_003_0419/week3/day12/WhileCountDown.java
@@ -0,0 +1,10 @@
+package com.example._003_0419.week3.day12;
+
+public class WhileCountDown {
+    public static void main(String[] args) {
+        int cnt = 5;
+        while (cnt > 0) {
+            System.out.println(cnt--);
+        }
+    }
+}
