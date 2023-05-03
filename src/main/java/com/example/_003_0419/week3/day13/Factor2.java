a0173f05a857a419c7cd3e1e72f0866874556a63
yezyaa <iyeahji@gmail.com>
2023-05-03 11:20:51 +0900

    feat: 약수 구하기 - Factor, Factor2, Factor3 추가

---
+++ src/main/java/com/example/_003_0419/week3/day13/Factor2.java
@@ -0,0 +1,12 @@
+package com.example._003_0419.week3.day13;
+
+public class Factor2 {
+    public static void main(String[] args) {
+        int num = 6;
+        for (int i = 1; i <= num ; i++) {
+            if (num % i == 0) {
+                System.out.printf("%d %% %d --> %d\n", num, i, num % i);
+            }
+        }
+    }
+}
