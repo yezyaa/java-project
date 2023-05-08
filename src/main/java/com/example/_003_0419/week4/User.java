6655bb065a28d25d299f85b195a481ffdd341e5e
yezyaa <iyeahji@gmail.com>
2023-05-08 11:34:37 +0900

    feat: 멤버변수와 연계 - User, UserTest 추가

---
+++ src/main/java/com/example/_003_0419/week4/User.java
@@ -0,0 +1,11 @@
+package com.example._003_0419.week4;
+
+public class User {
+    String name;
+    String phoneNumber; // 01012342345 하면 앞에 0이 짤림
+    int age;
+
+    boolean isAdult() {
+        return age >= 18;
+    }
+}
---
+++ src/main/java/com/example/_003_0419/week4/UserTest.java
@@ -0,0 +1,16 @@
+package com.example._003_0419.week4;
+
+public class UserTest {
+    public static void main(String[] args) {
+        User user1 = new User();
+        user1.name = "이예지";
+        user1.age = 14;
+
+        User user2 = new User();
+        user2.name = "김철수";
+        user2.age = 40;
+
+        System.out.printf("%s는 성인입니까? %s\n", user1.name, user1.isAdult());
+        System.out.printf("%s는 성인입니까? %s\n", user2.name, user2.isAdult());
+    }
+}
