c70dc5d63a74f672538288aaeaad4a11517a6d17
yezyaa <iyeahji@gmail.com>
2023-05-08 09:48:23 +0900

    feat: 오른쪽 직각삼각형 별 찍기 - RightTriangleStar 추가

---
+++ src/main/java/com/example/_003_0419/week4/RightTriangleStar.java
@@ -0,0 +1,16 @@
+package com.example._003_0419.week4;
+
+import java.util.Scanner;
+
+public class RightTriangleStar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt(); // 몇 줄인가?
+        for (int i = 0; i < input; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
