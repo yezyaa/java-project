783bd2c82fd6c81f11e69952b33a75cf0428c1cd
yezyaa <iyeahji@gmail.com>
2023-05-08 13:25:05 +0900

    feat: 여러가지 리턴 타입 - VariousReturnType 추가

---
+++ src/main/java/com/example/_003_0419/week4/VariousReturnType.java
@@ -0,0 +1,27 @@
+package com.example._003_0419.week4;
+
+public class VariousReturnType {
+    public boolean isAdult() {
+        return true;
+    }
+
+    public int plus() {
+        return 1 + 1;
+    }
+
+    public User getUser() {
+        return new User();
+    }
+
+    public void printHello() {
+        System.out.println("Hello");
+    }
+
+    public static void main(String[] args) {
+        VariousReturnType vrt = new VariousReturnType();
+        boolean isAdult = vrt.isAdult();
+        int plusResult = vrt.plus();
+        User user = vrt.getUser();
+        vrt.printHello();
+    }
+}
