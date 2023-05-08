2e8e0687123e990ea9df04e1a3258dd653c282dd
yezyaa <iyeahji@gmail.com>
2023-05-08 11:09:58 +0900

    feat: 메소드 - SpaceInvaders, SpaceInvadersTest 추가

---
+++ src/main/java/com/example/_003_0419/week4/SpaceInvaders.java
@@ -0,0 +1,13 @@
+package com.example._003_0419.week4;
+
+public class SpaceInvaders {
+    int location;
+
+    public void moveLeft() {
+        location -= 1;
+    }
+
+    public void moveRight() {
+        location += 1;
+    }
+}
---
+++ src/main/java/com/example/_003_0419/week4/SpaceInvadersTest.java
@@ -0,0 +1,19 @@
+package com.example._003_0419.week4;
+
+public class SpaceInvadersTest {
+    public static void main(String[] args) {
+        SpaceInvaders spaceInvaders = new SpaceInvaders();
+        spaceInvaders.moveLeft();
+        System.out.println(spaceInvaders.location);
+        spaceInvaders.moveLeft();
+        System.out.println(spaceInvaders.location);
+        spaceInvaders.moveLeft();
+        System.out.println(spaceInvaders.location);
+        spaceInvaders.moveRight();
+        System.out.println(spaceInvaders.location);
+        spaceInvaders.moveRight();
+        System.out.println(spaceInvaders.location);
+        spaceInvaders.moveRight();
+        System.out.println(spaceInvaders.location);
+    }
+}
