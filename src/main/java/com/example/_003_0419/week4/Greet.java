863289d3a8427a7c7377b9f198030801fd9f499a
yezyaa <iyeahji@gmail.com>
2023-05-08 11:22:49 +0900

    feat: 메소드 선언 및 실행(호출) - Greet, GreetTest 추가

---
+++ src/main/java/com/example/_003_0419/week4/Greet.java
@@ -0,0 +1,7 @@
+package com.example._003_0419.week4;
+
+public class Greet {
+    public void printHello() {
+        System.out.println("Hello");
+    }
+}
---
+++ src/main/java/com/example/_003_0419/week4/GreetTest.java
@@ -0,0 +1,8 @@
+package com.example._003_0419.week4;
+
+public class GreetTest {
+    public static void main(String[] args) {
+        Greet greet = new Greet();
+        greet.printHello();
+    }
+}
