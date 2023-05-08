2515126fb8ffb107cd96edde15bbff2435f4d384
yezyaa <iyeahji@gmail.com>
2023-05-08 15:48:34 +0900

    feat: 인수 출력 - ArgsPrinter 추가

---
+++ src/main/java/com/example/_003_0419/week4/day15/ArgsPrinter.java
@@ -0,0 +1,9 @@
+package com.example._003_0419.week4.day15;
+
+import java.util.Arrays;
+
+public class ArgsPrinter {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(args));
+    }
+}
