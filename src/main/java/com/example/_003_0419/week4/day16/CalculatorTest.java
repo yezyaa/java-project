43d14fc51423a8c6663b73fda2d4dd2f3acf6d11
yezyaa <iyeahji@gmail.com>
2023-05-09 11:28:58 +0900

    feat: 다른 클래스에서 Static 메소드 호출 - CalculatorTest, PreDefinedStaticMethod 추가

---
+++ src/main/java/com/example/_003_0419/week4/day16/CalculatorTest.java
@@ -0,0 +1,7 @@
+package com.example._003_0419.week4.day16;
+
+public class CalculatorTest {
+    public static void main(String[] args) {
+        Calculator.printMinus(10 ,20);
+    }
+}
---
+++ src/main/java/com/example/_003_0419/week4/day16/PreDefinedStaticMethod.java
@@ -0,0 +1,8 @@
+package com.example._003_0419.week4.day16;
+
+public class PreDefinedStaticMethod {
+    public static void main(String[] args) {
+        System.out.println(Integer.parseInt("30"));
+        System.out.println(Math.pow(2, 10));
+    }
+}
