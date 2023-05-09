ef45ed05344669da754b2e7d4c8c2a86d6b5f068
yezyaa <iyeahji@gmail.com>
2023-05-09 11:40:23 +0900

    feat: 리팩토링 - SeparateToMethod 추가

---
+++ src/main/java/com/example/_003_0419/week4/day16/SeparateToMethod.java
@@ -0,0 +1,25 @@
+package com.example._003_0419.week4.day16;
+
+import java.util.Arrays;
+
+public class SeparateToMethod {
+    public static void printArr(int[][] arr) {
+        System.out.println(Arrays.toString(arr[0]));
+        System.out.println(Arrays.toString(arr[1]));
+        System.out.println(Arrays.toString(arr[2]));
+        System.out.println("------------");
+    }
+    public static void main(String[] args) {
+        int[][] arr = {
+                {10, 20, 30},
+                {40, 50, 60},
+                {70, 80, 90}
+        };
+        printArr(arr);
+
+        arr[0][0] = 0;
+        arr[1][1] = 0;
+        arr[2][2] = 0;
+        printArr(arr);
+    }
+}
