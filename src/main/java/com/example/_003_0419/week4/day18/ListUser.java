aca71375415246a16ef376b1fe37c1885c73abf7
yezyaa <iyeahji@gmail.com>
2023-05-26 02:36:48 +0900

    feat: List 예제

---
+++ src/main/java/com/example/_003_0419/week4/day18/ListUser.java
@@ -0,0 +1,21 @@
+package com.example._003_0419.week4.day18;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListUser {
+    public static void main(String[] args) {
+        List<User> users = new ArrayList<>();
+
+        User user1 = new User("김미미", "010-1234-1234", 37);
+        users.add(user1);
+
+        User user2 = new User("김나나", "010-1234-1235", 14);
+        users.add(user2);
+
+        for (User user : users) {
+            System.out.printf("%s님의 전화번호는 %s입니다.\n", user.getName(), user.getPhoneNumber());
+        }
+
+    }
+}
---
+++ src/main/java/com/example/_003_0419/week4/day18/UserTest.java
@@ -0,0 +1,17 @@
+package com.example._003_0419.week4.day18;
+
+public class UserTest {
+    public static void main(String[] args) {
+        /*User user1 = new User();
+        user1.setName("김미미");
+        user1.setAge(14);
+
+        User user2 = new User();
+        user2.setName("김나나");
+        user2.setAge(37);
+
+        System.out.printf("%s는 어른인지? %b\n", user1.getName(), user1.isAdult());
+        System.out.printf("%s는 어른인지? %b\n", user2.getName(), user2.isAdult());*/
+    }
+
+}
