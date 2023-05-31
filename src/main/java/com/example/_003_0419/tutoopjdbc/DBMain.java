adbacaa221e164afac66292be07a48e1bb8bfcfa
yezy <iyeahji@gmail.com>
2023-05-31 14:39:08 +0900

    feat: Optional<Person>

--- src/main/java/com/example/_003_0419/tutoopjdbc/DBMain.java
+++ src/main/java/com/example/_003_0419/tutoopjdbc/DBMain.java
@@ -6,6 +6,7 @@ import com.example._003_0419.tutoopjdbc.model.Person;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class DBMain extends BaseDAO {
     public static void main(String[] args) {
@@ -19,7 +20,7 @@ public class DBMain extends BaseDAO {
 //        dbMain.getCodeName(); // world.db 테이블 데이터 가져오기
     }
 
-    private Person findByNamePerson(String pname) {
+    private Optional<Person> findByNamePerson(String pname) {
         String sql = "select id, name from person where name = ?";
         try {
             // create a database connection
@@ -30,14 +31,14 @@ public class DBMain extends BaseDAO {
             if (rs.next()) {
                 int id = rs.getInt("id");
                 String name = rs.getString("name");
-                return new Person(id, name);
+                return Optional.of(new Person(id, name));
             }
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
             close();
         }
-        return null;
+        return Optional.empty();
     }
 
     private List<Person> findAllPerson() {
