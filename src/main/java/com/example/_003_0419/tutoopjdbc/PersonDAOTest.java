788c5aec80523eebe5f543faece2b31850e7d2ff
yezy <iyeahji@gmail.com>
2023-05-31 15:10:34 +0900

    feat: PersonDAO 단위 테스트 추가(Optional 간단 테스트)

---
+++ src/main/java/com/example/_003_0419/tutoopjdbc/PersonDAOTest.java
@@ -0,0 +1,41 @@
+package com.example._003_0419.tutoopjdbc;
+
+import com.example._003_0419.tutoopjdbc.model.PersonDAO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+class PersonDAOTest {
+    PersonDAO personDAO = new PersonDAO();
+
+    @BeforeEach
+    void setUp() {
+        personDAO.initPerson();
+    }
+
+    @Test
+    void findByNamePerson() {
+        String name = "leo";
+        personDAO.insertPerson(name);
+        assertEquals(name, personDAO.findByNamePerson(name).get().getName());
+        assertNotEquals(name, personDAO.findByNamePerson("aa"));
+        assertEquals(Optional.empty(), personDAO.findByNamePerson("aa"));
+    }
+
+    @Test
+    void findAllPerson() {
+        personDAO.insertPerson("leo");
+        personDAO.insertPerson("yui");
+        assertEquals("[001 leo, 002 yui]", personDAO.findAllPerson().toString());
+    }
+
+    @Test
+    void insertPerson() {
+        int cnt = personDAO.insertPerson("leo");
+        assertEquals(1, cnt);
+    }
+}
