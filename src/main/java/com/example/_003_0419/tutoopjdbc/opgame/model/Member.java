1cfc2abb15796bb69668e079f4dfc29fb9aa8da8
yezy <iyeahji@gmail.com>
2023-06-01 11:32:47 +0900

    feat: Member DAO, DTO 구현

--- src/main/java/com/example/_003_0419/tutoopjdbc/opgame/model/Member.java
+++ src/main/java/com/example/_003_0419/tutoopjdbc/opgame/model/Member.java
@@ -1,12 +1,20 @@
 package com.example._003_0419.tutoopjdbc.opgame.model;
 
 public class Member {
+    String id;
+    boolean isLogined;
+
+    public Member(String id, boolean isLogined) {
+        this.id = id;
+        this.isLogined = isLogined;
+    }
+
     public String getID() {
-        return null;
+        return id;
     }
 
     public boolean isLogined() {
-        return false;
+        return isLogined;
     }
 }
 
--- src/main/java/com/example/_003_0419/tutoopjdbc/opgame/model/MemberDAO.java
+++ src/main/java/com/example/_003_0419/tutoopjdbc/opgame/model/MemberDAO.java
@@ -1,21 +1,111 @@
 package com.example._003_0419.tutoopjdbc.opgame.model;
 
+import java.sql.SQLException;
 import java.util.Optional;
 
 public class MemberDAO extends BaseDAO {
     public void initMember() {
+        String sql = """
+                CREATE TABLE opg_member (
+                seq INTEGER PRIMARY KEY AUTOINCREMENT,\s
+                id TEXT NOT NULL,
+                pw TEXT NOT NULL,
+                islogined INTEGER DEFAULT '0',
+                created_at TEXT DEFAULT CURRENT_TIMESTAMP,
+                updated_at TEXT DEFAULT CURRENT_TIMESTAMP
+                )
+                """;
+        try {
+            getConn();
+            smt = conn.createStatement();
+            smt.executeUpdate("DROP TABLE IF EXISTS opg_member");
+            smt.executeUpdate(sql);
+        }
+        catch(SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        finally {
+            close();
+        }
     }
 
     public Optional<Member> login(String id, String pw) {
+        String sql = """
+                update opg_member SET islogined='1' WHERE id=? and pw=?
+                """;
+        try {
+            getConn();
+            psmt = conn.prepareStatement(sql);
+            psmt.setString(1, id);
+            psmt.setString(2, pw);
+            int cnt = psmt.executeUpdate();
+            if (cnt > 0) {
+                psmt = conn.prepareStatement("SELECT id, islogined FROM opg_member WHERE id=?");
+                psmt.setString(1, id);
+                rs = psmt.executeQuery();
+                if (rs.next()) {
+                    String rid = rs.getString(1);
+                    boolean risLogined = rs.getBoolean(2);
+                    return Optional.of( new Member(rid, risLogined) );
+                }
+            }
+        }
+        catch(SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        finally {
+            close();
+        }
         return Optional.empty();
     }
 
-    public void join(String id, String pw) {
-
+    public int join(String id, String pw) {
+        int cnt = 0;
+        String sql = "insert into opg_member(id, pw) values(?, ?)";
+        try {
+            getConn();
+            psmt = conn.prepareStatement(sql);
+            psmt.setString(1, id);
+            psmt.setString(2, pw);
+            cnt = psmt.executeUpdate();
+        }
+        catch(SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        finally {
+            close();
+        }
+        return cnt;
     }
 
     public Optional<Member> logout(String id) {
-        return null;
+        String sql = """
+                update opg_member SET islogined='0' WHERE id=?
+                """;
+        try {
+            getConn();
+            psmt = conn.prepareStatement(sql);
+            psmt.setString(1, id);
+//            psmt.setString(2, pw);
+            int cnt = psmt.executeUpdate();
+            if (cnt > 0) {
+                psmt = conn.prepareStatement("SELECT id, islogined FROM opg_member WHERE id=?");
+                psmt.setString(1, id);
+                rs = psmt.executeQuery();
+                if (rs.next()) {
+                    String rid = rs.getString(1);
+                    boolean risLogined = rs.getBoolean(2);
+                    return Optional.of( new Member(rid, risLogined) );
+                }
+            }
+        }
+        catch(SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        finally {
+            close();
+        }
+        return Optional.empty();
     }
 }
 
